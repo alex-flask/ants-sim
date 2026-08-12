1e80a325353611dd298ac32567aeae9d32cbe180
Alex Kolbinov <alexkolbinov@gmail.com>
2026-08-12 15:01:47 +0300

    added field

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 #include <thread>
 #include <chrono>
 
+#define FIELD_SIZE 16
+
+void fill_field();
+void print_field();
+void newScreen();
+
+std::string field[FIELD_SIZE][FIELD_SIZE];
+
 int main()
 {
+    //MARK: setup
+    fill_field();
 
     while (true) {
         std::cout << "Hello, world!" << std::endl;
@@ -12,4 +22,20 @@ int main()
     }
 
     return 0;
+}
+
+void fill_field()
+{
+    int i, j;
+    for (i = 0; i < FIELD_SIZE; i++)
+        for (j = 0; j < FIELD_SIZE; j++)
+            field[i][j] = " ";
+}
+
+void print_field()
+{
+}
+
+void newScreen()
+{
 }
