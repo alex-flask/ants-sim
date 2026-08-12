9d1f74723cb88905abe4bdad969ee7049df9a306
Alex Kolbinov <alexkolbinov@gmail.com>
2026-08-12 15:38:09 +0300

    finished printing

--- printing.cpp
+++ printing.cpp
@@ -11,6 +11,12 @@ void fill_field(std::string field[FIELD_SIZE][FIELD_SIZE])
 
 void print_field(std::string field[FIELD_SIZE][FIELD_SIZE])
 {
+    int i, j;
+    for (i = 0; i < FIELD_SIZE; i++) {
+        for (j = 0; j < FIELD_SIZE; j++)
+            std::cout << field[i][j];
+        std::cout << std::endl;
+    }
 }
 
 void newScreen()
