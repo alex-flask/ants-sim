077e1c24aa151516d1cc4c3bfa71842076fb1502
Alex Kolbinov <alexkolbinov@gmail.com>
2026-08-12 14:48:29 +0300

    main loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+
+    while (true) {
+        std::cout << "Hello, world!" << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));       
+    }
+
     return 0;
 }
